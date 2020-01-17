f1029834b6a4d38ead0ab7869aa9017730513e17
student <niu.ula.a@gmail.com>
2020-01-17 17:45:29 +0100

    Url to File Fixed

--- DemoDrive/app/Http/Controllers/FileController.php
+++ DemoDrive/app/Http/Controllers/FileController.php
@@ -15,7 +15,7 @@ class FileController extends Controller
         $file->name = $request->file('file')->getClientOriginalName();
         $file->size = $request->file('file')->getSize()/1024;
         $file->type = $request->file('file')->getClientMimeType();
-        Storage::disk('s3')->put($file->name, $request->file('file'));
+        Storage::disk('s3')->put($file->name, file_get_contents($request->file('file')));
         $file->url =  Storage::disk('s3')->url($file->name);
         $file->save();
 
