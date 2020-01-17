34eb9a8207632599430f7408b698980578b99774
student <niu.ula.a@gmail.com>
2020-01-17 15:21:16 +0100

    Access Configuration S3

--- DemoDrive/config/filesystems.php
+++ DemoDrive/config/filesystems.php
@@ -57,10 +57,10 @@ return [
 
         's3' => [
             'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
+            'key' => env('AKIAJSVPLYRPWEYN36FA'),
+            'secret' => env('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'),
+            'region' => env('eu-west-3'),
+            'bucket' => env('quokadrive'),
             'url' => env('AWS_URL'),
         ],
 
