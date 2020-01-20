b27d86077e08ca6982fff3e4c31da74f0503cda9
8artek <barniii@poczta.onet.pl>
2020-01-20 18:54:46 +0100

    link sharing fixed

--- DemoDrive/app/Http/Controllers/FileController.php
+++ DemoDrive/app/Http/Controllers/FileController.php
@@ -17,7 +17,7 @@ class FileController extends Controller
         $file->size = $request->file('file')->getSize()/1024;
         $file->type = $request->file('file')->getClientMimeType();
         Storage::disk('s3')->put(Auth::user()->id . "/" . $file->name, file_get_contents($request->file('file')));
-        $file->url =  Storage::disk('s3')->url($file->name);
+        $file->url =  Storage::disk('s3')->url(Auth::user()->id . "/" . $file->name);
         $file->save();
 
         return redirect('/');
